8eb1c3dbc3b7d5dd9c2d23caf5c6b8e70db0dcfc
Jesus Pacheco <jesuspacheco0219@gmail.com>
2022-06-08 00:06:25 -0500

    close #9

--- solution.py
+++ solution.py
@@ -9,8 +9,9 @@ def tex_to_list(text_nodes):
 ListAds=[]
 for i in range(7096):
     ListAds.append([])
-# read ways and connect with his nodes        
+# read ways and connect with his nodes  and create ways map
 ways={}
+
 with open("waysWithNodes.txt") as f_obj:  
     for line in f_obj:
         k,v_text=line.strip().split('.')
@@ -27,10 +28,15 @@ with open("nodes.txt") as f_obj:
         index+=1
 
 #Create Nodes    
+waysIndexs=[]
+for i in range(1630):
+    waysIndexs.append([])
+ways_index_count=0    
+keepFirst=false
 for  way in ways:
-   keepFirst=false
    for node in ways[way]:
        current_index= number_to_index[node]
+       waysIndexs[ways_index_count].append(current_index)
        if keepFirst==true:
            ListAds[current_index].append(back_index)
            ListAds[back_index].append(current_index)
@@ -38,10 +44,19 @@ for  way in ways:
        if keepFirst==false:
          back_index= number_to_index[node]
          keepFirst=true
+   ways_index_count+=1     
 cont=0
+
 with open("AdsList","a") as file:  
     for  lista in  ListAds:
       file.write(f"{cont}:{str(lista)} \n")
       cont+=1
+
+contways=0
+with open("Ways_and_nodes","a") as file:  
+    for  lista in  waysIndexs:
+      file.write(f"{contways}:{str(lista)} \n")
+      contways+=1
+
     
 print("finish")        
